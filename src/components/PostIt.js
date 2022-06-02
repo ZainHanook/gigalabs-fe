70e558236de40bf75bf8384dd14f4da9ab79018e
ZainHanook <zainhanook99@gmail.com>
2022-06-02 20:13:55 +0500

    secondCommit

---
+++ src/components/PostIt.js
@@ -0,0 +1,38 @@
+import React, { useState } from 'react'
+import { Skeleton, Switch, Card, Avatar } from 'antd';
+import { EditOutlined, EllipsisOutlined, SettingOutlined } from '@ant-design/icons';
+import { iamges } from './images';
+const { Meta } = Card;
+
+export const PostIt = () => {
+    // const [loading, setLoading] = useState(true);
+
+    // const onChange = (checked) => {
+    //     console.log(checked)
+    //     setLoading(!checked);
+    // };
+    return (
+        <Card
+            style={{
+                width: 300,
+            }}
+            // cover={
+            //     <img
+            //         alt="example"
+            //         src=''
+            //     />
+            // }
+            actions={[
+                <SettingOutlined key="setting" />,
+                <EditOutlined key="edit" />,
+                <EllipsisOutlined key="ellipsis" />,
+            ]}
+        >
+            <Meta
+                avatar={<Avatar size="small" src={iamges.avatar1} />}
+                title="Card title"
+                description="This is the description"
+            />
+        </Card>
+    )
+}
---
+++ src/components/images/index.js
@@ -0,0 +1,4 @@
+import avatar1 from "../images/avatar1.png";
+export const iamges = {
+    avatar1
+}
